b930325f77871c77a4fa245fce595ce31fba0178
Rocco Nori <norirocco11@gmail.com>
2023-12-05 18:23:37 +0100

    base con pattern funzionante.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,4 +52,5 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation("androidx.core:core-splashscreen:1.0.0")
+    implementation("androidx.cardview:cardview:1.0.0")
 }
