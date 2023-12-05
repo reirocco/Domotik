4190841416e5fb0e8cb3db156a2e86f07d7666b0
Rocco Nori <norirocco11@gmail.com>
2023-12-05 21:32:23 +0100

    carico icone vettoriali

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,9 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    // PERSONALI
     implementation("androidx.core:core-splashscreen:1.0.0")
     implementation("androidx.cardview:cardview:1.0.0")
+
 }
+
