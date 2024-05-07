315827082dd62eac233da2f67eec015accf6e735
matty <s1095052@studenti.univpm.it>
2024-05-07 11:19:24 +0200

    Aggiunta e modifica ActivityLight

--- app/src/main/java/com/example/domotik/ui/lights/LightsActivity.kt
+++ app/src/main/java/com/example/domotik/ui/lights/LightsActivity.kt
@@ -8,6 +8,7 @@ import android.widget.ImageButton
 import com.example.domotik.R
 
 class LightsActivity : AppCompatActivity() {
+    val lights = mutableListOf<Boolean>(false,false,false,false)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lights)
@@ -19,12 +20,48 @@ class LightsActivity : AppCompatActivity() {
         actionbar.setDisplayHomeAsUpEnabled(true)
 
     }
-    override fun onSupportNavigateUp(): Boolean {
+    /* override fun onSupportNavigateUp(): Boolean {
         onBackPressed()
         return true
+    } */
+
+    fun SwitchOnCucina1(view: View) {
+        val light = lights[0]
+        lights[0] = !light
+        if(!light){
+            findViewById<ImageButton>(R.id.button1).setImageResource(R.drawable.baseline_light_mode_24)
+        } else {
+            findViewById<ImageButton>(R.id.button1).setImageResource(R.drawable.baseline_emoji_objects_24)
+        }
     }
 
-    fun SwitchOnCucina(view: View) {
-        findViewById<ImageButton>(R.id.button1).setImageResource(R.drawable.baseline_light_mode_24)
+    fun SwitchOnCucina2(view: View) {
+        val light = lights[1]
+        lights[1] = !light
+        if(!light){
+            findViewById<ImageButton>(R.id.button2).setImageResource(R.drawable.baseline_light_mode_24)
+        } else {
+            findViewById<ImageButton>(R.id.button2).setImageResource(R.drawable.baseline_emoji_objects_24)
+        }
+    }
+
+    fun SwitchOnCucina3(view: View) {
+        val light = lights[2]
+        lights[2] = !light
+        if(!light){
+            findViewById<ImageButton>(R.id.button3).setImageResource(R.drawable.baseline_light_mode_24)
+        } else {
+            findViewById<ImageButton>(R.id.button3).setImageResource(R.drawable.baseline_emoji_objects_24)
+        }
+    }
+
+    fun SwitchOnCucina4(view: View) {
+        val light = lights[3]
+        lights[3] = !light
+        if(!light){
+            findViewById<ImageButton>(R.id.button4).setImageResource(R.drawable.baseline_light_mode_24)
+        } else {
+            findViewById<ImageButton>(R.id.button4).setImageResource(R.drawable.baseline_emoji_objects_24)
+        }
     }
 }
--- app/src/main/java/com/example/domotik/ui/util/ApiConf.kt
+++ app/src/main/java/com/example/domotik/ui/util/ApiConf.kt
@@ -1,3 +1,3 @@
 package com.example.domotik.ui.util
 
-const val apiKey:String=""  // your API KEy for openweathermap.org
+const val apiKey:String="f6a678b23b4d74c2d736d34aae30f6dc"  // your API KEy for openweathermap.org
